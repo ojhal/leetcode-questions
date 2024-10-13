23515269dd4fabb7b4ceb3cfcc082d809466f1b9
Ojhal Patwa <ojhal.patwa.taskmonk.ai>
2024-10-13 16:55:06 +0530

    refoctor

--- Array/twoSum2.js
+++ Array/twoSum2.js
@@ -5,17 +5,17 @@
  */
 function twoSum(numbers, target) {
     let left = 0; 
-    let right = numbers.length - 1; 
+    let right = numbers.length - 1
     
     while (left < right) {
-        const sum = numbers[left] + numbers[right];
+        const sum = numbers[left] + numbers[right]
         
         if (sum === target) {
-            return [left + 1, right + 1];
+            return [left + 1, right + 1]
         } else if (sum < target) {
-            left++;
+            left++
         } else {
-            right--;
+            right--
         }
     }
     
