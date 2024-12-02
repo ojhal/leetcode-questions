97731b120f348979e497a1f4994574e68bcefca2
Ojhal Patwa <ojhal.patwa.taskmonk.ai>
2024-12-02 08:56:54 +0530

    2778. Sum of Squares of Special Elements

---
+++ Array/SumOfSquares.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfSquares = function(nums) {
+    let sum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums.length % (i + 1) === 0) {
+      sum += nums[i] * nums[i];
+    }
+  }
+
+  return sum;
+};
+console.log(sumOfSquares([1, 2, 3, 4]));
+console.log(sumOfSquares([2, 7, 1, 19, 18, 3]));
