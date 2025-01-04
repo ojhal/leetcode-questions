68afe995dcfefce7fb6b935f1c7d5517e4ab2517
Ojhal Patwa <ojhal.patwa.taskmonk.ai>
2025-01-04 20:55:23 +0530

    delay

--- src/Promises/Sleeps.js
+++ src/Promises/Sleeps.js
@@ -10,3 +10,15 @@ function sleep(millis) {
    * let t = Date.now()
    * sleep(100).then(() => console.log(Date.now() - t)) // 100
    */
+
+  //Write a function delay that returns a promise. And that promise should return a setTimeout that calls resolve after 1000ms
+
+const delay = () => {
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve("hey there!")
+        },1000)
+    })
+}
+
+delay().then((result)=> console.log(result))
